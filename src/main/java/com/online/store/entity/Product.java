cc71854454cacb8e1d6ce6aa0d687e8edf2404e8
Mark Salumaa <msalumaa@gmail.com>
2021-05-15 13:19:41 +0300

    Create Product.java
    Created Product entity

---
+++ src/main/java/com/online/store/entity/Product.java
@@ -0,0 +1,43 @@
+package com.online.store.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "thumbnail")
+    private String thumbnail;
+
+    @Column(name = "price")
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "product_type_id")
+    private ProductType productType;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+}
