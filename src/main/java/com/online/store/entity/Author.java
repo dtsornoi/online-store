ae3d5b22c88f79db0fed1a8507889c67fdf85cd3
DMITRI TŠORNÕI <47533288+dtsornoi@users.noreply.github.com>
2021-05-15 14:35:01 +0300

    Update Author.java

--- src/main/java/com/online/store/entity/Author.java
+++ src/main/java/com/online/store/entity/Author.java
@@ -1,5 +1,6 @@
 package com.online.store.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,7 @@ import javax.persistence.*;
 @Entity
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class Author {
 
     @Id
