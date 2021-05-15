714785b0b1bf64501fd3db0a7b40de73c337caaf
DMITRI TŠORNÕI <47533288+dtsornoi@users.noreply.github.com>
2021-05-15 13:05:15 +0300

    update:  UserAccount entity

--- src/main/java/com/online/store/entity/UserAccount.java
+++ src/main/java/com/online/store/entity/UserAccount.java
@@ -1,4 +1,29 @@
 package com.online.store.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/**
+ * Entity class UserAccount
+ *
+ * @author Dmitry Tsornoi
+ */
+@Entity
+@Table(name = "user_account")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    private String login;
+
+    private String password;
 }
