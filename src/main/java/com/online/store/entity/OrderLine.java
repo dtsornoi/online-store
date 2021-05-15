d9bdc5b135066cb037474bb1f9f78f98b53b5f44
DMITRI TŠORNÕI <47533288+dtsornoi@users.noreply.github.com>
2021-05-15 14:38:02 +0300

    refactor: Removed RequiredArgsConstructor

--- src/main/java/com/online/store/entity/OrderLine.java
+++ src/main/java/com/online/store/entity/OrderLine.java
@@ -1,5 +1,6 @@
 package com.online.store.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
@@ -16,7 +17,7 @@ import javax.persistence.*;
 @Entity
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class OrderLine {
 
     @Id
