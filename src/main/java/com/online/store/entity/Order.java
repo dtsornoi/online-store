39d907e8b62f48dcbaa65e5b405e3ca3a771f31b
abcdzz <50486608+abcdzz@users.noreply.github.com>
2021-05-15 13:38:23 +0300

    Order class
    Added Order class

---
+++ src/main/java/com/online/store/entity/Order.java
@@ -0,0 +1,50 @@
+package com.online.store.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Entity class Order
+ *
+ * @author Vladimir
+ */
+@Entity
+@Table(name = "user_account")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "username")
+    String userName;
+
+    @Column(name = "total_cost")
+    double totalCost;
+
+    @Column(name = "delivery_address")
+    String deliveryAddress;
+
+    @Column(name = "date_of_order")
+    Date dateOfOrder;
+
+    @OneToMany(mappedby = "cart")
+    private List<OrderLine> orderLine;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+
+    @Column(name = "status")
+    String status;
+
+
+}
