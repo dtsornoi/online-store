132a1108641f729eb10a69a011711f979f5a320d
DMITRI TŠORNÕI <47533288+dtsornoi@users.noreply.github.com>
2021-05-18 21:36:14 +0300

    add: roles.service

--- src/main/frontend/online-store/src/app/service/roles.service.ts
+++ src/main/frontend/online-store/src/app/service/roles.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
 
+const URL = 'http://localhost:8080/api/role'
+
+/**
+ * Service for Role retrieving and persisting in Backend
+ *
+ * @author Dmitri Tsornoi
+ */
 @Injectable({
   providedIn: 'root'
 })
 export class RolesService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+
+  getAllRoles(): Observable<any> {
+    return this.http.get(`${URL}/`);
+  }
+
+  getRoleById(id): Observable<any>{
+    return this.http.get(`${URL}/${id}`);
+  }
+
+  addRole(role): Observable<any>{
+    return this.http.post(`${URL}/`, role);
+  }
+
+  updateRole(role): Observable<any>{
+    return this.http.put(`${URL}/`, role);
+  }
+
+  deleteRole(role): Observable<any> {
+    return this.http.post(`${URL}/delete/`, role);
+  }
+
+  restore(role): Observable<any> {
+    return this.http.post(`${URL}/restore/`, role);
+  }
 }
+
