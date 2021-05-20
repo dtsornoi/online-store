fee65e939ff20e9a04800747e55d9eeca4fdc6e6
msalu <msalumaa@gmail.com>
2021-05-20 10:45:20 +0300

    Service layer (Angular)
    Added service for Category, OrderLine, Orders, Product

--- src/main/frontend/online-store/src/app/service/order-line.service.ts
+++ src/main/frontend/online-store/src/app/service/order-line.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const URL = 'http://localhost:8080/api/order-line'
+
+/**
+ * Service for OrderLine retrieveing and persisting in backend
+ * 
+ * @author Mark Salumaa
+ */
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderLineService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<any>{
+    return this.http.get(`${URL}/`);
+  }
+
+  get(id): Observable<any>{
+    return this.http.get(`${URL}/${id}`);
+  }
+
+  create(customer): Observable<any>{
+    return this.http.post(`${URL}/`, customer);
+  }
+
+  update(customer): Observable<any>{
+    return this.http.put(`${URL}/`, customer);
+  }
+
+  delete(customer): Observable<any>{
+    return this.http.post(`${URL}/delete`, customer);
+  }
+
+  restore(customer): Observable<any>{
+    return this.http.post(`${URL}/restore`, customer);
+  }
+
 }
--- src/main/frontend/online-store/src/app/service/product.service.ts
+++ src/main/frontend/online-store/src/app/service/product.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+const URL = 'http://localhost:8080/api/product'
+
+/**
+ * Service for Product retrieveing and persisting in backend
+ * 
+ * @author Mark Salumaa
+ */
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<any>{
+    return this.http.get(`${URL}/`);
+  }
+
+  get(id): Observable<any>{
+    return this.http.get(`${URL}/${id}`);
+  }
+
+  create(customer): Observable<any>{
+    return this.http.post(`${URL}/`, customer);
+  }
+
+  update(customer): Observable<any>{
+    return this.http.put(`${URL}/`, customer);
+  }
+
+  delete(customer): Observable<any>{
+    return this.http.post(`${URL}/delete`, customer);
+  }
+
+  restore(customer): Observable<any>{
+    return this.http.post(`${URL}/restore`, customer);
+  }
+
+
 }
