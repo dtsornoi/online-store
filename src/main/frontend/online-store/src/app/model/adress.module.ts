cad3a956ec30a9d33698d623e598bffa79bc9c58
abcdzz <50486608+abcdzz@users.noreply.github.com>
2021-05-20 19:36:20 +0300

    Frontend add: Address module and service

--- src/main/frontend/online-store/src/app/model/adress.module.ts
+++ src/main/frontend/online-store/src/app/model/adress.module.ts
@@ -1,2 +1,15 @@
+/**
+ * Model for backend Address POJO
+ * 
+ * @author Vladimir 
+ */
+
 export class Adress {
+    id?: number;
+    city?: string;
+    code?: string;
+    country?: string;
+    is_active?: boolean;
+    street?: string;
+    zip?: string;
 }
