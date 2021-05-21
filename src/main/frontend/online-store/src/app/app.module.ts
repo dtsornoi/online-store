1311cadc5cad2933fad0a4a3ae9d24e8eb593578
Dmitri Tsornoi <dmitri.tsornoi@gmail.com>
2021-05-21 15:03:58 +0300

    update: app.module.ts

--- src/main/frontend/online-store/src/app/app.module.ts
+++ src/main/frontend/online-store/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ProductComponent } from './components/product/product.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { CartComponent } from './components/cart/cart.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -27,7 +28,8 @@ import { CartComponent } from './components/cart/cart.component';
     ProductComponent,
     LoginComponent,
     RegisterComponent,
-    CartComponent
+    CartComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
